bff76402700f1ad4c62e78a7911f41c1948b6e29
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2026-01-23 04:40:00 -0500

    auto save

--- update_package.bat
+++ update_package.bat
@@ -1,2 +1,3 @@
 poetry run pip uninstall -y dcc
 poetry run pip install ./dist/dcc-0.1.0-cp312-cp312-win_amd64.whl
+poetry run python -c "from dcc import Point; p = Point(0, 0); print(p.GetCoordinates())"
