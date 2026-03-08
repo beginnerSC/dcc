17209efb9e5c7911a3ef2bb140a73d970d570140
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2026-03-07 22:00:00 -0500

    auto save

--- cpp/include/dcc_core/vector.h
+++ cpp/include/dcc_core/vector.h
@@ -1,58 +1,44 @@
 #ifndef DCC_CORE_VECTOR_H
 #define DCC_CORE_VECTOR_H
 
-#include <stdexcept>
-#include <iostream>
-
 class Vector {
 private:
   int* a_;
   size_t size_;
   size_t capacity_;
 
-  // Helper method to allocate and initialize array
   int* GetInitializedArray(size_t capacity);
 
 public:
-  // Constructors and destructors
   Vector();
   explicit Vector(int size);
   Vector(const Vector& other);
   Vector(Vector&& other);
   ~Vector();
 
-  // Iterator class
-  class iterator {
+  class Iterator {
     int* ptr_;
 
   public:
-    explicit iterator(int* ptr) noexcept;
+    explicit Iterator(int* ptr) noexcept;
     int& operator*() const noexcept;
     int* operator->() const noexcept;
-    iterator& operator++() noexcept;
-    iterator operator++(int) noexcept;
-    bool operator==(iterator other) const noexcept;
-    bool operator!=(iterator other) const noexcept;
-    int* GetPtr() const noexcept;
+    Iterator& operator++() noexcept;
+    Iterator operator++(int) noexcept;
+    bool operator==(Iterator other) const noexcept;
+    bool operator!=(Iterator other) const noexcept;
   };
 
-  // Iterator methods
-  iterator begin() const noexcept;
-  iterator end() const noexcept;
-
-  // Assignment operators
-  Vector& operator=(const Vector& other) noexcept;
-  Vector& operator=(Vector&& other) noexcept;
-
-  // Subscript operators
-  int& operator[](size_t i) noexcept;
-  const int& operator[](size_t i) const noexcept;
-
-  // Public methods
+  Iterator begin() const noexcept;
+  Iterator end() const noexcept;
   void Print();
   void PushBack(const int& val);
   void Resize(const size_t& size);
   size_t Size() const noexcept;
+  Vector& operator=(const Vector& other) noexcept;
+  Vector& operator=(Vector&& other) noexcept;
+  int& operator[](size_t i) noexcept;
+  const int& operator[](size_t i) const noexcept;
 };
 
 #endif
